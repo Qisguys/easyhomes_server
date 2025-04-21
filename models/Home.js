66014843e3d4cda9fe8af69390810de4ae16f56c
Qisguys <122888167+Qisguys@users.noreply.github.com>
2025-04-21 12:37:57 +0530

    Update Home.js

--- models/Home.js
+++ models/Home.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
+const imageSchema = new mongoose.Schema({
+  data: Buffer,       // Store image data as a Buffer
+  contentType: String, // Store MIME type (e.g., image/png, image/jpeg)
+  filename: String,    // Store original filename
+}, { _id: false }); // Don't create an _id for the image subdocument
+
 const homeSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  name: { type: String, required: true },  
+  name: { type: String, required: true },
   mobile: { type: String, required: true },
   street: { type: String, required: true },
   town: { type: String, required: true },
@@ -10,11 +16,10 @@ const homeSchema = new mongoose.Schema({
   pincode: { type: String, required: true },
   pluscode: { type: String, required: true },
   rentprice: { type: String, required: true },
-  images: [{ type: String, required: true }],
+  images: [imageSchema], // Array of images with Buffer data
   renter: { type: mongoose.Schema.Types.ObjectId, ref: "Renter", required: true },
   commits: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Commit' }],
-
-});
+}, { timestamps: true });
 
 const Home = mongoose.model("Home", homeSchema);
 module.exports = Home;
